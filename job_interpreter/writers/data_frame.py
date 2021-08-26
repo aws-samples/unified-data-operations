b08ea18113a89609ec7fcb2355d0877e89cf377d
mlewk <mlewk@amazon.de>
2021-08-26 12:17:13 +0200

    Update Glue catalog; Propagate partition key.

--- job_interpreter/writers/data_frame.py
+++ job_interpreter/writers/data_frame.py
@@ -10,11 +10,22 @@ class DataFrameWriter:
         self.context = context
 
     def write(self, data_frame: DataFrame, options: dict):
-        dynamic_df = DynamicFrame.fromDF(data_frame, self.context, "dynamic_df")
+        dynamic_df = DynamicFrame.fromDF(data_frame, self.context, 'dynamic_df')
         self.context.write_dynamic_frame.from_options(
             frame=dynamic_df,
             connection_type="s3",
             connection_options={'path': options['location']},
             format=options['stored_as'],
-            transformation_ctx="datasink2"
+            transformation_ctx='datasink2',
+            additional_options=resolve_additional_options(options)
         )
+
+
+def resolve_additional_options(options: dict):
+    additional_options = {'enableUpdateCatalog': True}
+
+    partition_key = options.get('partition_by')
+    if partition_key:
+        additional_options['partitionKeys'] = [partition_key]
+
+    return additional_options
