2c68b90adae9fda8a0e4d4f9251663710ef619e0
Csaba Tamas <csatam@amazon.com>
2021-09-01 00:52:10 +0200

    fixing ci / python backwards compatibility

--- tasks/custom_business_logic.py
+++ tasks/custom_business_logic.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from pyspark.sql.functions import concat, col, lit
 
 from driver.common import get_data_set
 from driver.task_executor import DataSet
 
 
-def execute(inp_dfs: list[DataSet]):
+def execute(inp_dfs: List[DataSet]):
     ds = get_data_set(inp_dfs, 'person')
     if ds:
         ds.df = ds.df.withColumn('full_name', concat(col('first_name'), lit(' '), col('last_name')))
