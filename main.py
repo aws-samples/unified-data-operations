77d040dd0128454315668b1b3789f96055eacff6
Csaba Tamas <csatam@amazon.com>
2021-09-01 02:50:26 +0200

    param handling

--- main.py
+++ main.py
@@ -11,24 +11,28 @@ from driver.io_handlers import lake_input_handler, lake_output_handler, connecti
 
 
 def init_aws(args):
-    driver.aws_provider.init(profile=args.aws_profile, region=args.aws_region)
+    profile = None
+    region = None
+    if hasattr(args, 'aws_profile'):
+        profile = args.aws_profile
+    if hasattr(args, 'aws_region'):
+        region = args.aws_region
+    driver.aws_provider.init(profile=profile, region=region)
 
 
-def init_system(product_path: str):
+def init_system(product_def_path: str):
     driver.io_handlers.init(driver.aws_provider.connection_provider)
     conf = SparkConf()
-    if args.aws_profile:
+    if hasattr(args, 'aws_profile'):
         print(f'Setting aws profile: {args.aws_profile}')
         os.environ["AWS_PROFILE"] = args.aws_profile
         conf.set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.profile.ProfileCredentialsProvider")
-    if args.local:
+    if hasattr(args, 'local') and args.local:
         deps_path = f'{os.path.dirname(os.path.abspath(__file__))}/spark_deps'
         pgres_driver_jars = f'{deps_path}/postgresql-42.2.23.jar'
         local_jars = [pgres_driver_jars]
         if args.jars:
             local_jars.extend([f'{deps_path}/{j}' for j in args.jars.strip().split(',')])
-        # aws_jars = f'{deps_path}/aws-java-sdk-bundle-1.11.375.jar'
-        # hadoop_jar = f'{deps_path}/hadoop-aws-3.2.0.jar'
         jars = ','.join(local_jars)
         conf.set("spark.jars", jars)
     driver.init(spark_config=conf)
@@ -36,21 +40,23 @@ def init_system(product_path: str):
     driver.register_postprocessors(schema_checker, constraint_processor, transformer_processor)
     driver.register_output_handler('default', lake_output_handler)
     driver.register_output_handler('lake', lake_output_handler)
-    driver.process_product(f'{os.path.dirname(os.path.abspath(__file__))}{product_path}')
+    driver.process_product(f'{os.path.dirname(os.path.abspath(__file__))}/{product_def_path}')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
     parser.add_argument('--JOB_NAME', help='the name of this pyspark job')
     parser.add_argument('--product_path', help='the data product definition folder')
     parser.add_argument('--aws_profile', help='the AWS profile to be used for connection')
     parser.add_argument('--aws_region', help='the AWS region to be used')
     parser.add_argument('--local', action='store_true', help='local development')
     parser.add_argument('--jars', help='extra jars to be added to the Spark context')
+    parser.add_argument('--additional-python-modules', help='this is used by Glue, ignored by this code')
     args = parser.parse_args()
     print(f'PATH: {os.environ["PATH"]}')
     print(f'SPARK_HOME: {os.environ.get("SPARK_HOME")}')
     print(f'PYTHONPATH: {os.environ.get("PYTHONPATH")}')
 
     init_aws(args)
-    init_system(f'{args.product_path}{os.path.sep if not args.product_path.endswith(os.path.sep) else None}')
+    product_path = args.product_path if hasattr(args, 'product_path') else './'
+    init_system(f'{product_path}{os.path.sep if not product_path.endswith(os.path.sep) else ""}')
