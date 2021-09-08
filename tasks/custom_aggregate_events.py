ac4aeee2814c5feb31c6a1bd2e71f2114593f707
Fabian Fuelling <fafuell@amazon.de>
2021-09-08 07:29:01 +0200

    Product C: Create new aggregated dataset "calendar" from three input data products

--- tasks/custom_aggregate_events.py
+++ tasks/custom_aggregate_events.py
@@ -1,14 +1,23 @@
-import time
-import datetime
 from typing import List
 
-from pyspark.sql.functions import concat, col, lit, unix_timestamp
+from pyspark.sql.functions import col
+
 from driver.common import get_data_set
-from driver.task_executor import DataSet
+from driver.core import DataSet
 
 
-def execute(inp_dfs: List[DataSet], create_timestamp=False):
-    ds = get_data_set(inp_dfs, 'person')
-    if ds:
-        ds.df = ds.df.withColumn('full_name', concat(col('last_name'), lit(', '), col('first_name')))
-    return inp_dfs
+def execute(inp_dfs: List[DataSet]):
+    events = get_data_set(inp_dfs, 'events').df.alias('events')
+    teams = get_data_set(inp_dfs, 'teams').df.alias('teams')
+    locations = get_data_set(inp_dfs, 'locations').df.alias('locations')
+
+    events = events.join(locations, on=events.location_id == locations.id).select('events.*', col("locations.name").alias('location'))
+    events = events.join(teams, on=events.home_team_id == teams.id).select('events.*', col('location'), col("teams.name").alias('home_team'))
+    events = events.join(teams, on=events.away_team_id == teams.id).select('events.*', col('location'), col('home_team'), col("teams.name").alias('away_team'))
+
+    events = events.drop(col('location_id'))
+    events = events.drop(col('home_team_id'))
+    events = events.drop(col('away_team_id'))
+
+    output_ds = DataSet(model_id='calendar', df=events, input_id=None, model=None, product=None)
+    return [output_ds]
