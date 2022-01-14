ca6b246205413cbea1ea616cf5ebb3ffeef157af
Csaba Tamas <csatam@amazon.com>
2022-01-15 00:25:17 +0100

    changed model: added extends keyword, input is simplified, engine and schema version is added, owner is moved

--- tests/assets/integration_sport_events/tasks/custom_aggregate_events.py
+++ tests/assets/integration_sport_events/tasks/custom_aggregate_events.py
@@ -2,14 +2,14 @@ from typing import List
 
 from pyspark.sql.functions import col
 
-from driver.common import get_data_set
+from driver.common import find_dataset_by_id
 from driver.core import DataSet
 
 
 def execute(inp_dfs: List[DataSet]):
-    events = get_data_set(inp_dfs, 'events').df.alias('events')
-    teams = get_data_set(inp_dfs, 'teams').df.alias('teams')
-    locations = get_data_set(inp_dfs, 'locations').df.alias('locations')
+    events = find_dataset_by_id(inp_dfs, 'events').df.alias('events')
+    teams = find_dataset_by_id(inp_dfs, 'teams').df.alias('teams')
+    locations = find_dataset_by_id(inp_dfs, 'locations').df.alias('locations')
 
     events = events.join(locations, on=events.location_id == locations.id).select('events.*', col("locations.name").alias('location'))
     events = events.join(teams, on=events.home_team_id == teams.id).select('events.*', col('location'), col("teams.name").alias('home_team'))
