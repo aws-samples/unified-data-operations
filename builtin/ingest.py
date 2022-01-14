2be4e7e985f28fd5083f48061be23125b4bde5c3
Csaba Tamas <csatam@amazon.com>
2022-01-15 00:27:02 +0100

    ingest is part of the built-in transform library

--- builtin/ingest.py
+++ builtin/ingest.py
@@ -1,8 +1,14 @@
 from typing import List
-
+import time
+import datetime
 from driver.task_executor import DataSet
+from pyspark.sql.functions import lit, unix_timestamp
 
 
 def execute(inp_dfs: List[DataSet], create_timestamp=False):
     print(f'create timestamp: {create_timestamp}')
+    if create_timestamp:
+        timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        for ds in inp_dfs:
+            ds.df = ds.df.withColumn('time', unix_timestamp(lit(timestamp), 'yyyy-MM-dd HH:mm:ss').cast("timestamp"))
     return inp_dfs
