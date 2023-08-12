d166e5bb7267aef17ffc185c3767fb4fef90c495
Csaba Tamas <csatam@amazon.com>
2023-08-12 23:59:48 +0200

    source code generation framework is ready

--- driver/driver.py
+++ driver/driver.py
@@ -18,15 +18,12 @@ def get_spark() -> SparkSession:
     if __SPARK__:
         return __SPARK__
     else:
-        raise RuntimeError('Spark Session is not created yet. Call init() first.')
+        raise RuntimeError("Spark Session is not created yet. Call init() first.")
 
 
 def get_or_create_session(config=None) -> SparkSession:  # pragma: no cover
     """Build spark session for jobs running on cluster."""
-    spark = SparkSession.builder.appName(__name__) \
-        .config(conf=config) \
-        .enableHiveSupport() \
-        .getOrCreate()
+    spark = SparkSession.builder.appName(__name__).config(conf=config).enableHiveSupport().getOrCreate()
 
     return spark
 
@@ -34,10 +31,10 @@ def get_or_create_session(config=None) -> SparkSession:  # pragma: no cover
 def init(spark_session: SparkSession = None, spark_config=None):
     global __SPARK__
     if not spark_session:
-        logger.info('creating a new Spark session.')
+        logger.info("creating a new Spark session.")
         __SPARK__ = get_or_create_session(spark_config)
     else:
-        logger.info('returning already existing Spark session.')
+        logger.info("returning already existing Spark session.")
         __SPARK__ = spark_session
     # sc  = __SPARK__.sparkContext
     # sc.setSystemProperty("com.amazonaws.services.s3.enableV4", "true")
@@ -46,12 +43,12 @@ def init(spark_session: SparkSession = None, spark_config=None):
 def install_dependencies(product_path: str):
     new_packages = packager.install_dependencies(product_path)
     if new_packages:
-        logger.info(f'packaging up the following new dependencies {new_packages.keys()}')
+        logger.info(f"packaging up the following new dependencies {new_packages.keys()}")
         for new_pack_name in new_packages.keys():
             zipfile = ziplib(new_packages.get(new_pack_name), new_pack_name)
-            logger.info(f'-----> installing {zipfile}')
+            logger.info(f"-----> installing {zipfile}")
             get_spark().sparkContext.addPyFile(zipfile)
-        logger.debug('=> Dependencies are installed.')
+        logger.debug("=> Dependencies are installed.")
 
 
 def process_product(args, product_path: str):
